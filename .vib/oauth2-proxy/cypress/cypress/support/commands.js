a5840c427593949fb49cebe67f7be721c4b55b02
Juan Ariza Toledano <juan.ariza@broadcom.com>
2025-10-07 07:58:45 +0200

    [bitnami/oauth2-proxy] feat: add support for TLS (#813)

--- .vib/oauth2-proxy/cypress/cypress/support/commands.js
+++ .vib/oauth2-proxy/cypress/cypress/support/commands.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: APACHE-2.0
  */
 
-const BASE_URL = 'http://vmware-oauth2-proxy.my';
+const BASE_URL = 'https://vmware-oauth2-proxy.my';
 
 // DEX is deployed at localhost:5556/dex/, which is not exposed (only port 80 is).
 // A proxy pass in localhost/dex/ is configured to allow communication with it, but
