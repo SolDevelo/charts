fbd5687f318874fdfc616027c9b80b36c285b76a
Juan Ariza Toledano <juan.ariza@broadcom.com>
2025-10-07 09:01:20 +0200

    [bitnami/neo4j] ci: Change test endpoint - ii (#824)
    Signed-off-by: juan131 <juan.ariza@broadcom.com>

--- .vib/neo4j/cypress/cypress/support/commands.js
+++ .vib/neo4j/cypress/cypress/support/commands.js
@@ -4,7 +4,7 @@
  */
 
 const COMMAND_DELAY = 2000;
-const BASE_URL = 'http://bitnami-neo4j.my';
+const BASE_URL = 'http://neo4j.com';
 
 for (const command of ['click']) {
   Cypress.Commands.overwrite(command, (originalFn, ...args) => {
