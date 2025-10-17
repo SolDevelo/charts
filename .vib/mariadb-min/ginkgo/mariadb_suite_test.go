41d10f44ad683c0d10a836c2815458efba0164c4
Juan Ariza Toledano <juan.ariza@broadcom.com>
2025-10-17 20:09:48 +0200

    [bitnami/mariadb-min] feat: standardize TLS (#1444)
    Signed-off-by: juan131 <juan.ariza@broadcom.com>

--- .vib/mariadb-min/ginkgo/mariadb_suite_test.go
+++ .vib/mariadb-min/ginkgo/mariadb_suite_test.go
@@ -3,6 +3,7 @@ package mariadb_test
 import (
 	"context"
 	"flag"
+	"fmt"
 	"testing"
 	"time"
 
@@ -77,7 +78,7 @@ func createJob(ctx context.Context, c kubernetes.Interface, name, port, image, s
 						{
 							Name:    "mariadb",
 							Image:   image,
-							Command: []string{"mysql", "-h$(MARIADB_HOST)", "-p$(MARIADB_PASSWORD)", "-u$(MARIADB_USERNAME)", "-P$(MARIADB_PORT)", "-e", stmt},
+							Command: []string{"mariadb", "-h$(MARIADB_HOST)", "-p$(MARIADB_PASSWORD)", "-u$(MARIADB_USERNAME)", "-P$(MARIADB_PORT)", "--ssl", "--ssl-ca=/certs/ca.crt", "--ssl-verify-server-cert=off", "-e", stmt},
 							Env: []v1.EnvVar{
 								{
 									Name:  "MARIADB_PASSWORD",
@@ -97,6 +98,22 @@ func createJob(ctx context.Context, c kubernetes.Interface, name, port, image, s
 								},
 							},
 							SecurityContext: securityContext,
+							VolumeMounts: []v1.VolumeMount{
+								{
+									Name:      "tls-certs",
+									MountPath: "/certs",
+								},
+							},
+						},
+					},
+					Volumes: []v1.Volume{
+						{
+							Name: "tls-certs",
+							VolumeSource: v1.VolumeSource{
+								Secret: &v1.SecretVolumeSource{
+									SecretName: fmt.Sprintf("%s-crt", stsName),
+								},
+							},
 						},
 					},
 				},
